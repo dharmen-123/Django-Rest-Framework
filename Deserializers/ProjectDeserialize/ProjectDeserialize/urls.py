eeb098093e6d82bebdf4f525672687f094b03711
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-18 17:33:27 +0530

    commit

--- Deserializers/ProjectDeserialize/ProjectDeserialize/urls.py
+++ Deserializers/ProjectDeserialize/ProjectDeserialize/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app2nd import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('studentapi/',views.studentapi,name="studentapi"),
 ]
--- Deserializers/ProjectDeserialize/app2nd/views.py
+++ Deserializers/ProjectDeserialize/app2nd/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Student
+from.serializers import StudentSerializer
+from django.http import JsonResponse , HttpResponse
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+import io
 
 # Create your views here.
+
+def studentapi(req):
+    json_data=req.body
+    if json_data:
+        stream=io.BytesIO(json_data)
+        pythondata=JSONParser().parse(stream)
+        x=pythondata['id']
+        data = Student.objects.filter(id=x)
+        if data :
+            showdata=Student.objects.get(id=x)
+            serializer=StudentSerializer(showdata)
+            return JsonResponse(serializer.data , safe=True)
+        else :
+            msg={'msg':'Data is not found in database'}
+            return JsonResponse(msg) 
+    else :
+        Alldata=Student.objects.all()
+        serializer=StudentSerializer(Alldata,many=True)
+        return JsonResponse(serializer.data ,safe=False)
+    # return HttpResponse("data not found")
