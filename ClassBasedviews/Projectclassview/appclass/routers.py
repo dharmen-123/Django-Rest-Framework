18984d00c0dc504e17bf681f62feb3fa4921001b
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-23 17:27:16 +0530

    commit

--- ClassBasedviews/Projectclassview/appclass/routers.py
+++ ClassBasedviews/Projectclassview/appclass/routers.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from .views import StudentViewSet
 
-router = routers.SimpleRouter()
+# router = routers.SimpleRouter()
+router=routers.DefaultRouter()
 router.register(r'students', StudentViewSet)
 urlpatterns = router.urls
