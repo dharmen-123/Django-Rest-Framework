0eeb681346a231da22d1f9c03f192937b10121d7
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-24 17:36:56 +0530

    commit

--- ClassBasedviews/Projectclassview/Projectclassview/settings.py
+++ ClassBasedviews/Projectclassview/Projectclassview/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'appclass',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+
     ]
 }
