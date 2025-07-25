c8d65ae8a7995158afdaedfa701e4c4506e20e54
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-25 16:58:49 +0530

    Token Authentication

--- ClassBasedviews/Projectclassview/appclass/views.py
+++ ClassBasedviews/Projectclassview/appclass/views.py
@@ -3,7 +3,7 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import mixins
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 # Create your views here.
 
  ################# MIXIN BASED VIEWS ###############
@@ -58,6 +58,6 @@ class StudentViewSet(viewsets.ModelViewSet):
     """
     A simple ViewSet for viewing and editing accounts.
     """
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
