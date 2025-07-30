db973679c964741d95dd4140ee9d92055eee31d0
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-30 12:52:15 +0530

    commit

--- practiseclassbasedviews/Projectclasses/Projectclasses/urls.py
+++ practiseclassbasedviews/Projectclasses/Projectclasses/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 from appclass import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('student/', views.StudentList.as_view()),
+    path('student/<int:pk>/', views.StudentDetail.as_view()),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- practiseclassbasedviews/Projectclasses/appclass/views.py
+++ practiseclassbasedviews/Projectclasses/appclass/views.py
@@ -2,6 +2,47 @@ from django.shortcuts import render, redirect
 from .models import Student
 from .serializers import StudentSerializer
 
-
 # Create your views here.
 
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class StudentList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    def get(self, request, format=None):
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu ,many=True)
+        return Response(serializer.data)
+
+    def post(req):
+        serializer = StudentSerializer(data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class StudentDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get(req, pk):
+        stu  = Student.objects.get(id=pk)
+        serializer = StudentSerializer(stu )
+        return Response(serializer.data)
+
+    def put(req,pk):
+        stu  = Student.objects.get(id=pk)
+        serializer = StudentSerializer(stu, data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(req,pk):
+        stu = Student.objects.get(id=pk)
+        stu.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
