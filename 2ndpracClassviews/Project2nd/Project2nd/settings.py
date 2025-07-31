0f1a17bce0c8f61a2f974d6d7b9ece1a186b7632
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-31 23:57:58 +0530

    commit

--- 2ndpracClassviews/Project2nd/Project2nd/settings.py
+++ 2ndpracClassviews/Project2nd/Project2nd/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app2nd',
     'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
