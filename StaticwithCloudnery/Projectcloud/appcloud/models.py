fd11156658849d025424300637e2a389707abfa8
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-01 22:08:19 +0530

    commit

--- StaticwithCloudnery/Projectcloud/appcloud/models.py
+++ StaticwithCloudnery/Projectcloud/appcloud/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    password=models.CharField()
+    cpassword=models.CharField()
+    image=models.ImageField(upload_to='file/')
+    
--- StaticwithCloudnery/Projectcloud/appcloud/views.py
+++ StaticwithCloudnery/Projectcloud/appcloud/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
 
 def home(req):
     return render(req,'home.html')
+
+def form(req):
+    if req.method=='POST':
+        name=req.POST.get('name')
+        email=req.POST.get('email')
+        password=req.POST.get('password')
+        cpassword=req.POST.get('confirm')
+        image=req.FILES.get('image')
+
+        Student.objects.create(name=name,email=email,password=password,cpassword=cpassword,image=image)
+        msg = 'thanks'
+        return render(req,'home.html',{'msg':msg})
+
+    else:
+        msg = 'plrace fill the form'
+        return render(req,'home.html',{'msg':msg})
