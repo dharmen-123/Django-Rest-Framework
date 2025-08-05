0bc4c47c07f2d85a614f00a77da39b3819f07f5b
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-05 17:08:55 +0530

    CSV format

--- DjangoCSVFile/ProjectCSV/appcsv/views.py
+++ DjangoCSVFile/ProjectCSV/appcsv/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 from .models import Employee
+import csv
+import io
 
 # Create your views here.
+
+
+def upload(req):
+    return render(req,'home.html')
+    
