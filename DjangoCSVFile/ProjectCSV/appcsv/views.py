9e093cf3cc3c6ed29f8f039430198e5a47a32ffb
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-05 17:16:49 +0530

    CSV file data

--- DjangoCSVFile/ProjectCSV/appcsv/views.py
+++ DjangoCSVFile/ProjectCSV/appcsv/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from .models import Employee
 import csv
@@ -8,5 +8,19 @@ import io
 
 
 def upload(req):
-    return render(req,'home.html')
-    
+    if req.method=='POST':
+        csvfile=req.FILES['csvfile']
+        decoded_file=csvfile.read().decode('utf-8')
+        iostring=io.StringIO(decoded_file)
+        reader=csv.DictReader(iostring)
+        for row in reader:
+            Employee.objects.create(
+                name=row['name'],
+                email=row['email'],
+                contact=row['contact'],
+                department=row['department'],
+                salary=row['salary'],
+            )
+        return render(req,'home.html')
+    else:
+        return render(req,'home.html')
