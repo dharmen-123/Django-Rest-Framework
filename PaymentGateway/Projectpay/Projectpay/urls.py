3b262a66f06c00c8ffbb7e1bbe563e96e287ec67
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-20 22:50:48 +0530

    commit

--- PaymentGateway/Projectpay/Projectpay/urls.py
+++ PaymentGateway/Projectpay/Projectpay/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from apppay import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('',views.home,name='home'),
+    path('payment/',views.payment,name='payment'),
+    path('paymenthandle/',views.paymenthandle,name='paymenthandle'),
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- PaymentGateway/Projectpay/apppay/views.py
+++ PaymentGateway/Projectpay/apppay/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import razorpay
 
 # Create your views here.
+
+
+def home(request):
+    return render(request,'home.html')
+
+def payment(request):
+    pass
+
+def paymenthandle(request):
+    pass
