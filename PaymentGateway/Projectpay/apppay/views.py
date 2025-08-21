2dc5a7cb4d6fc21e68ac25a5e97385b827722221
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-21 22:35:31 +0530

    commit

--- PaymentGateway/Projectpay/apppay/views.py
+++ PaymentGateway/Projectpay/apppay/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import razorpay
-
+from .models import Payment
 # Create your views here.
 
 def home(request):
@@ -9,7 +9,7 @@ def home(request):
 
 def payment(request):
     if request.method=='POST':
-        amount=request.POST.get('amount')*100
+        amount=int(2000*100)
         client=razorpay.Client(auth=("rzp_test_8MpcoTaUXnGlMQ","wz2Q1xWs4LueA8LZwxIBMuPR"))
         order_data={
             'amount':amount,
@@ -18,6 +18,11 @@ def payment(request):
             'payment_capture':1
         }
         order=client.order.create(data=order_data)
+        Payment.objects.create(
+            order_id=order["id"],
+            amount=amount,
+            status="Created"
+            )
         payment = {
             "order_id": order["id"],
             "amount": amount,
@@ -27,6 +32,20 @@ def payment(request):
         }    
         return render(request,'home.html',{'payment':payment})    
     return render(request,'home.html')
-    
+
 def paymenthandle(request):
-    pass
+    if request.method=='POST':
+        client = razorpay.Client(auth=("rzp_test_8MpcoTaUXnGlMQ", "wz2Q1xWs4LueA8LZwxIBMuPR"))
+        params_dict = {
+            'razorpay_order_id': request.POST.get('razorpay_order_id'),
+            'razorpay_payment_id': request.POST.get('razorpay_payment_id'),
+            'razorpay_signature': request.POST.get('razorpay_signature')
+        }
+        # Verify the payment signature
+        client.utility.verify_payment_signature(params_dict)
+        payment = Payment.objects.get(order_id=request.POST.get('razorpay_order_id'))
+        payment.payment_id = request.POST.get('razorpay_payment_id')
+        payment.signature = request.POST.get('razorpay_signature')
+        payment.status = "Paid"
+        payment.save()
+        return render(request,'home.html',{'success':1})
