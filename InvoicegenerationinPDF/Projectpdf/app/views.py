3129c6be3edf177dc347a8d59a55bd6c6988a59a
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-25 23:25:36 +0530

    commit values

--- InvoicegenerationinPDF/Projectpdf/app/views.py
+++ InvoicegenerationinPDF/Projectpdf/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
 import openpyxl
 from django.template.loader import get_template
@@ -7,7 +7,11 @@ from xhtml2pdf import pisa
 
 def home(request):
     return render(request,'home.html')
-    
+
 def exportpdf(request):
-    
+    cdata=datetime.now()
+    date=cdata.strftime()
+    template='pdf.html'
+    data=User.objects
+
     pass
