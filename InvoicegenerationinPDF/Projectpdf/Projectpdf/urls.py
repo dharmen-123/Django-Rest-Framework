534fac945d45d710a713ba0fe3f1817a7edf609a
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-25 23:17:05 +0530

    commit

--- InvoicegenerationinPDF/Projectpdf/Projectpdf/urls.py
+++ InvoicegenerationinPDF/Projectpdf/Projectpdf/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('exportpdf/',views.exportpdf,name='exportpdf'),
-    
+
 ]
--- InvoicegenerationinPDF/Projectpdf/app/views.py
+++ InvoicegenerationinPDF/Projectpdf/app/views.py
@@ -5,6 +5,9 @@ import openpyxl
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 
+def home(request):
+    return render(request,'home.html')
+    
 def exportpdf(request):
     
     pass
