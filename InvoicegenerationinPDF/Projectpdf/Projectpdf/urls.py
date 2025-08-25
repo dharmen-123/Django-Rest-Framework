641fb015fb683c65f6db9169d6bc6defcd36cb9d
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-25 23:12:45 +0530

    pdfgeneration

--- InvoicegenerationinPDF/Projectpdf/Projectpdf/urls.py
+++ InvoicegenerationinPDF/Projectpdf/Projectpdf/urls.py
@@ -20,5 +20,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
+    path('exportpdf/',views.exportpdf,name='exportpdf'),
     
 ]
--- InvoicegenerationinPDF/Projectpdf/app/views.py
+++ InvoicegenerationinPDF/Projectpdf/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+import openpyxl
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+
+def exportpdf(request):
+    
+    pass
