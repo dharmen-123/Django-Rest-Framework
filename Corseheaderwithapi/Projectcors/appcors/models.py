3f76ee98d69b8edf794577c1d56d72bd3304cd1a
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-29 23:24:22 +0530

    cors

--- Corseheaderwithapi/Projectcors/appcors/models.py
+++ Corseheaderwithapi/Projectcors/appcors/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Alumini(models.Model):
     name=models.CharField(max_length=150)
     empid=models.CharField(max_length=150)
-    mobile=models.IntegerField()
+    mobile=models.CharField(max_length=200)
     designation=models.CharField()
     salary=models.CharField()
     city=models.CharField()
