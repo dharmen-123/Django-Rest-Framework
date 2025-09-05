4998ce8e93ea40da61b287e42d97f81ac2c77b14
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-09-05 22:26:18 +0530

    jwt token

--- JWTAuthentication/ProjectJWT/app/models.py
+++ JWTAuthentication/ProjectJWT/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name=models.CharField(max_length=200)
+    email=models.EmailField()
+    city=models.CharField(max_length=200)
+    contact=models.IntegerField()
+
+    class Meta:
+        db_table='Student'
